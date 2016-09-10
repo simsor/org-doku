0d7ca69daee5dacbcefb26a33e6d96ac3ab15a4f
Simon Garrelou <simon.garrelou@riseup.net>
2016-09-10 22:15:45 +0200

    Add check to see if the config file exists

--- common.py
+++ common.py
@@ -1,15 +1,20 @@
 
 import dokuwiki
+import sys
 
 def get_config():
     result = {}
-    with open("config.ini", "r") as f:
-        for line in f:
-            line = line.strip()
-            key, value = line.split("=")
-
-            result[key] = value
-
+    try:
+        with open("config.ini", "r") as f:
+            for line in f:
+                line = line.strip()
+                key, value = line.split("=")
+                
+                result[key] = value
+    except FileNotFoundError:
+        print("Config file doesn't exist. See README.md for how to create it.")
+        sys.exit(1)
+              
     return result
 
 
@@ -19,5 +24,6 @@ def connect_wiki(url, user, password):
         wiki = dokuwiki.DokuWiki(url, user, password)
     except (dokuwiki.DokuWikiError, Exception) as err:
         print("Error connecting to DokuWiki: %s" % (err))
+        sys.exit(1)
 
     return wiki
